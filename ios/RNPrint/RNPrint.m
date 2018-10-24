a2dd55fa8440ce43e8baeacc7d6280c1b703f0f2
Denis Polyakov <den1s.polyakov1@gmail.com>
2018-10-24 17:34:04 +0300

    pass view coordinates

--- ios/RNPrint/RNPrint.m
+++ ios/RNPrint/RNPrint.m
@@ -17,67 +17,67 @@ RCT_EXPORT_MODULE();
 RCT_EXPORT_METHOD(print:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     if (options[@"filePath"]){
         _filePath = [RCTConvert NSString:options[@"filePath"]];
     }
-    
+
     if (options[@"html"]){
         _htmlString = [RCTConvert NSString:options[@"html"]];
     }
-    
+
     if (options[@"printerURL"]){
         _printerURL = [NSURL URLWithString:[RCTConvert NSString:options[@"printerURL"]]];
         _pickedPrinter = [UIPrinter printerWithURL:_printerURL];
     }
-    
+
     if(options[@"isLandscape"]) {
         _isLandscape = [[RCTConvert NSNumber:options[@"isLandscape"]] boolValue];
     }
-    
+
     if ((_filePath && _htmlString) || (_filePath == nil && _htmlString == nil)) {
         reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Must provide either `html` or `filePath`. Both are either missing or passed together"));
     }
-    
+
     NSData *printData;
     BOOL isValidURL = NO;
     NSURL *candidateURL = [NSURL URLWithString: _filePath];
     if (candidateURL && candidateURL.scheme && candidateURL.host)
         isValidURL = YES;
-    
+
     if (isValidURL) {
         // TODO: This needs updated to use NSURLSession dataTaskWithURL:completionHandler:
         printData = [NSData dataWithContentsOfURL:candidateURL];
     } else {
         printData = [NSData dataWithContentsOfFile: _filePath];
     }
-    
+
     if(!_htmlString && ![UIPrintInteractionController canPrintData:printData]) {
         reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Unable to print this filePath"));
         return;
     }
-    
+
     UIPrintInteractionController *printInteractionController = [UIPrintInteractionController sharedPrintController];
     printInteractionController.delegate = self;
-    
+
     // Create printing info
     UIPrintInfo *printInfo = [UIPrintInfo printInfo];
-    
+
     printInfo.outputType = UIPrintInfoOutputGeneral;
     printInfo.jobName = [_filePath lastPathComponent];
     printInfo.duplex = UIPrintInfoDuplexLongEdge;
     printInfo.orientation = _isLandscape? UIPrintInfoOrientationLandscape: UIPrintInfoOrientationPortrait;
-    
+
     printInteractionController.printInfo = printInfo;
     printInteractionController.showsPageRange = YES;
-    
+
     if (_htmlString) {
         UIMarkupTextPrintFormatter *formatter = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:_htmlString];
         printInteractionController.printFormatter = formatter;
     } else {
         printInteractionController.printingItem = printData;
     }
-    
+
     // Completion handler
     void (^completionHandler)(UIPrintInteractionController *, BOOL, NSError *) =
     ^(UIPrintInteractionController *printController, BOOL completed, NSError *error) {
@@ -88,7 +88,7 @@ RCT_EXPORT_METHOD(print:(NSDictionary *)options
             resolve(completed ? printInfo.jobName : nil);
         }
     };
-    
+
     if (_pickedPrinter) {
         [printInteractionController printToPrinter:_pickedPrinter completionHandler:completionHandler];
     } else if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) { // iPad
@@ -100,13 +100,14 @@ RCT_EXPORT_METHOD(print:(NSDictionary *)options
 }
 
 
-RCT_EXPORT_METHOD(selectPrinter:(RCTPromiseResolveBlock)resolve
+RCT_EXPORT_METHOD(selectPrinter:(NSDictionary *)options
+                  resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-    
+
     UIPrinterPickerController *printPicker = [UIPrinterPickerController printerPickerControllerWithInitiallySelectedPrinter: _pickedPrinter];
-    
+
     printPicker.delegate = self;
-    
+
     void (^completionHandler)(UIPrinterPickerController *, BOOL, NSError *) =
     ^(UIPrinterPickerController *printerPicker, BOOL userDidSelect, NSError *error) {
         if (!userDidSelect && error) {
@@ -124,10 +125,12 @@ RCT_EXPORT_METHOD(selectPrinter:(RCTPromiseResolveBlock)resolve
             }
         }
     };
-    
+
     if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) { // iPad
         UIView *view = [[UIApplication sharedApplication] keyWindow].rootViewController.view;
-        [printPicker presentFromRect:view.frame inView:view animated:YES completionHandler:completionHandler];
+        float x = [options[@"x"] floatValue];
+        float y = [options[@"y"] floatValue];
+        [printPicker presentFromRect:CGRectMake(x, y, 0, 0) inView:view animated:YES completionHandler:completionHandler];
     } else { // iPhone
         [printPicker presentAnimated:YES completionHandler:completionHandler];
     }
